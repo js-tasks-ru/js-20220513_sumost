e10f49f4cbd7b3e98fa5715169a4b0a0b50556eb
Igor Const <sumost@yandex.ru>
2022-05-15 07:24:27 +0300

    clean code

--- 01-intro/1-sum/index.js
+++ 01-intro/1-sum/index.js
@@ -5,16 +5,11 @@
  * @returns {number}
  */
 export default function sum(m, n) {
-
-    //help function to check parameter is numeric
-    function isValidNumber (num) {
+     function isValidNumber (num) {
         return typeof num === 'number' && !isNaN(num);
     }
-
-
     if (isValidNumber(m) && isValidNumber(n)) 
         return (m + n);
     else 
         document.write("Проверте что у функции числовые параметры! <br/>");
- 
     }
