5c07d6f70d22ecfce6f5dda3df585d710f4e8bd7
Igor Const <sumost@yandex.ru>
2022-05-20 17:15:22 +0300

    add omit realisation

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,15 +5,13 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-    const sortedArr = [];
-    Array.prototype.push.apply(sortedArr, arr);
-     if (param === 'asc') {
+    const sortedArr = arr.slice();
+    if (param === 'asc') {
          let collator = new Intl.Collator(['ru', 'en'], { caseFirst: "upper" });
          sortedArr.sort(function (a,b) {
              return collator.compare(a,b);
          }, )
-     }
-    if (param === 'desc') {
+     } else {
         let collator = new Intl.Collator(['ru', 'en'], { caseFirst: "lower" });
         sortedArr.sort(function (a,b) {
              return collator.compare(b,a);
