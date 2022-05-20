5a85c240de6e5f4a20b78873f81c471cbf5ec710
Igor Const <sumost@yandex.ru>
2022-05-20 13:54:42 +0300

    add pick realisation ver.1

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -4,6 +4,12 @@
  * @param {...string} fields - the properties paths to pick
  * @returns {object} - returns the new object
  */
-export const pick = (obj, ...fields) => {
-
+export function pick (obj, ...fields)  {
+    const args = Array.from(arguments).slice(1);
+    const filteredObj = Object.keys(obj)
+        .filter(key => args.includes(key))
+        .reduce((newobj, key) => {
+        newobj[key] = obj[key];
+        return newobj;
+    }, {});
 };
