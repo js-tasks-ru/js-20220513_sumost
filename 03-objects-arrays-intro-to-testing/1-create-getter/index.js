1e01a3f7a08253deaf2683587bbe3daf43ce0908
Igor Const <sumost@yandex.ru>
2022-05-23 18:30:10 +0300

    add createGetter function by module_3

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,35 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+    const checkPath = path.split('.');
+    return function (obj) {
+        const isObject = function (o) {
+            if (o === null) {
+                return false;
+            }
+            return (typeof o === 'object');
+        };
+        let pathStr= [];
+        let resStr = '';
+        const objProps = function (o) {
+            for (let val in o) {
+                console.log(`===${val}===`);
+                pathStr.push(val);
+                if (isObject(o[val])) {
+                    objProps(o[val]);
+                } else {
+                    if (pathStr.join('.') === path) {
+                        resStr = o[val];
+                    }
+                }
+            };
+            return resStr;
+        };
+        const result = objProps(obj);
+        if (result !== '') {
+            return result;
+        } else {
+            return undefined;
+        }
+    }; 
 }
