72a33432daf0ffcd6b4425df8e1a6c2b9d59be9c
Igor Const <sumost@yandex.ru>
2022-05-24 11:51:47 +0300

    add invertObj function by module 3.2

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,20 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-
+        const isObject = function (o) {
+            if (o === null) {
+                return false;
+            }
+            return (typeof o === 'object');
+        };
+        if (!isObject(obj)) {
+            return undefined;
+        }
+        if (Object.values(obj).includes(undefined)) {
+             return {};
+        }
+        if (obj === {}) {
+            return {};
+        }
+        return Object.fromEntries( Object.entries(obj).map(item => item.reverse()) );
 }
